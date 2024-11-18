62eb2dce1a2669e5dcb6e2acd31579bc149d2ce7
Stefanie Vicente <stefanie.vicente@gmail.com>
2024-11-18 12:37:16 +0100

    update top bar css

--- src/components/DrinksTopBar.tsx
+++ src/components/DrinksTopBar.tsx
@@ -4,7 +4,27 @@ const Drinks = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-template-rows: repeat(1, 1fr);
-  gap: 0px;
+  background: #fc8744;
+  height: 100%;
+  align-items: center;
+  justify-items: center;
+`;
+
+const DrinkItem = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  font-weight: bold;
+  cursor: pointer;
+  background-color: #f75c03;
+  width: 80%;
+  height: 80%;
+
+  &:hover {
+    transform: scale(1.05);
+    opacity: 0.9;
+  }
 `;
 
 const DrinksTopBar = () => {
@@ -17,13 +37,9 @@ const DrinksTopBar = () => {
   return (
     <Drinks>
       {drinkTypes.map((option: string) => (
-        <div
-          style={{ border: "2px solid black", padding: "10px" }}
-          key={option}
-          onClick={() => handleClick(option)}
-        >
-          <p style={{ fontSize: 20 }}>{option}</p>
-        </div>
+        <DrinkItem key={option} onClick={() => handleClick(option)}>
+          {option}
+        </DrinkItem>
       ))}
     </Drinks>
   );
