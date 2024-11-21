5794ab85d1b3379b3f46ef05ac244d87ce53a12f
Stefanie Vicente <stefanie.vicente@gmail.com>
2024-11-21 17:25:05 +0100

    fix tests config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setupTests.ts',
+  },
 });
