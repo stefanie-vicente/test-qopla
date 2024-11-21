be10043d7f4a90f60166c05a63ed482ece65a7d8
Stefanie Vicente <stefanie.vicente@gmail.com>
2024-11-21 17:26:21 +0100

    prettier

--- src/components/Menu/MenuLateralBar.tsx
+++ src/components/Menu/MenuLateralBar.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import styled from "styled-components";
+import { useState } from 'react';
+import styled from 'styled-components';
 
 //  Components with the $isInactive prop are styled in this way because the functionality for these items is not implemented.
 export const Sidebar = styled.div`
@@ -23,11 +23,11 @@ export const MenuItem = styled.div<{
   cursor: pointer;
   font-size: 18px;
   background: ${(props) => {
-    if (props.$isSelected) return "#9F7BB3";
-    if (props.$isInactive) return "#555";
-    return "#6c4f3d";
+    if (props.$isSelected) return '#9F7BB3';
+    if (props.$isInactive) return '#555';
+    return '#6c4f3d';
   }};
-  color: ${(props) => (props.$isInactive ? "#aaa" : "#f5f5f5")};
+  color: ${(props) => (props.$isInactive ? '#aaa' : '#f5f5f5')};
   width: 80%;
   height: 80%;
   border-radius: 5px;
@@ -40,25 +40,24 @@ export const MenuItem = styled.div<{
 
   &:hover {
     opacity: 0.9;
-    ${(props) => !props.$isInactive && "transform: scale(1.05);"}
+    ${(props) => !props.$isInactive && 'transform: scale(1.05);'}
     outline: 2px solid #ffcc00;
   }
 
   &:active {
-    background-color: ${(props) => (props.$isSelected ? "#7a39b6" : "#6c4f3d")};
+    background-color: ${(props) => (props.$isSelected ? '#7a39b6' : '#6c4f3d')};
   }
 `;
 
 const MenuLateralBar = () => {
-  // move this to store to render other components
   const [selectedOption, setSelectedOption] = useState<string | null>(null);
 
   const menuOptions: string[] = [
-    "Meals",
-    "Sides",
-    "Drinks", // The only active option, others are inactive for this test purpose
-    "Desserts",
-    "Sauces",
+    'Meals',
+    'Sides',
+    'Drinks', // The only active option, others are inactive for this test purpose
+    'Desserts',
+    'Sauces',
   ];
 
   const handleClick = (item: string) => {
@@ -71,13 +70,13 @@ const MenuLateralBar = () => {
         <MenuItem
           key={option}
           onClick={() => handleClick(option)}
-          $isInactive={option !== "Drinks"}
-          $isSelected={option === "Drinks"}
+          $isInactive={option !== 'Drinks'}
+          $isSelected={option === 'Drinks'}
           role="menuitem"
           aria-selected={option === selectedOption}
-          aria-disabled={option !== "Drinks"}
+          aria-disabled={option !== 'Drinks'}
         >
-          {option !== "Drinks" ? `${option} is inactive` : "Drinks"}
+          {option !== 'Drinks' ? `${option} is inactive` : 'Drinks'}
         </MenuItem>
       ))}
     </Sidebar>
